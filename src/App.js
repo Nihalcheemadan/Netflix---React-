8e63ff925b6989d37d172ee11aa7f9fb6690ccfa
Nihal <nihalcheemadan12345@gmail.com>
2023-01-12 14:55:14 +0530

    React Netflix Homepage UI Completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import NavBar from "./Components/NavBar/NavBar";
+import './App.css'
+import Banner from "./Components/Banner/Banner";
+import RowPost from "./Components/RowPost/RowPost";
+import { actions, comedy, documentaries, horror, originals, trending } from "./urls";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar/>
+      <Banner/>
+      <RowPost url={originals} title='Netflix Originals' />
+      <RowPost url={trending} title='Trending Movies' isSmall  />
+      <RowPost url={actions} title='Action Movies' isSmall  />
+      <RowPost url={comedy} title='Comedy Movies' isSmall  />
+      <RowPost url={horror} title='Horror Movies' isSmall  />
+      <RowPost url={documentaries} title='Documentaries' isSmall  />
     </div>
   );
 }
---
+++ src/Components/Constants/Constants.js
@@ -0,0 +1,4 @@
+export const baseUrl = 'https://api.themoviedb.org/3'
+export const API_KEY = '26a425ee27120c0beab475798dc0fcb6'
+export const imageUrl = 'https://image.tmdb.org/t/p/original'
+
